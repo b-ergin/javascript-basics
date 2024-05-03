fd45fa7a218656238363232060a7bd7262974c8c
Burak Ergin <burakergincyp@gmail.com>
2024-05-03 19:07:32 +0300

    Completed Module

--- src/objects.js
+++ src/objects.js
@@ -1,41 +1,50 @@
 const createPerson = (name, age) => {
-  // your code here
-};
+  return {
+    name: name,
+    age: age
+}};
 
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object[property];
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+  return object.hasOwnProperty(property);
 };
 
 const isOver65 = person => {
-  // your code here
+  return person.age > 65;
 };
 
 const getAges = people => {
-  // your code here
+  return people.map(person => person.age);
 };
 
 const findByName = (name, people) => {
-  // your code here
+  return people.find(person => person.name === name);
 };
 
 const findHondas = cars => {
-  // your code here
+  return cars.filter(car => car.manufacturer === 'Honda');
 };
 
 const averageAge = people => {
-  // your code here
+  const totalAge = people.reduce((acc, person) => acc + person.age, 0);
+  return totalAge / people.length;
 };
 
 const createTalkingPerson = (name, age) => {
-  // your code here
+  return {
+    name: name,
+    age: age,
+    introduce: function(otherPerson) {
+      return `Hi ${otherPerson}, my name is ${this.name} and I am ${this.age}!`;
+    }
+  };
 };
 
 module.exports = {
